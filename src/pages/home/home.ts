0c3e2065c181c01a7c6e5c48beaf767a475cf167
Vincent Audet <audetvincent@gmail.com>
2018-10-03 21:10:28 -0400

    Add character page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  characters: Array<{id: number, name: string}>
 
   constructor(public navCtrl: NavController) {
+    this.characters = [];
+
+  }
+
+  create() {
+    this.characters.push({ id: 1, name: 'something' });
+    this.navCtrl.push('CharacterPage');
+  }
+
+  settings() {
 
   }
 
