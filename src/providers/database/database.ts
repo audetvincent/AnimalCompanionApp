4ebb332637e16844aedbf5a835811e8b54cbe9e9
vaudet <audetvincent@gmail.com>
2018-10-13 12:08:37 -0400

    Working logic for sql importation, now need to refined

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -19,30 +19,62 @@ export class DatabaseProvider {
 
   constructor(private http: Http, private sqlite: SQLite, private sqlitePorter: SQLitePorter, private platform: Platform, private storage: Storage) {
     this.databaseReady = new BehaviorSubject(false);
-    this.platform.ready().then(() =>{
+    this.platform.ready().then(() => {
       this.sqlite.create({
-        name: 'dnd35-monster.db',
+        name: 'monster.db',
         location: 'default'
       })
       .then((db: SQLiteObject) => {
         this.database = db;
-        this.storage.get('database_filled').then(val => {
-          if(val) {
-            this.databaseReady.next(true);
-          } else {
-            this.fillDatabase();
-          }
-        });
-      }, (error) => {
-        console.log('SQLite CREATE', error);
-      })
+        this.http.get('assets/monsters.sql')
+        .map(res => res.text())
+        .subscribe(sql => {
+          console.log('SQL MAPPED: ', sql);
+          this.sqlitePorter.importSqlToDb(this.database, sql)
+            .then(() => {
+              console.log('DATABASE SET');
+              this.databaseReady.next(true);
+            })
+            .catch((error) => {
+              console.log('ERROR: Import failed', error);
+            })
+        })
+      });
+      //   this.storage.get('database_filled').then(val => {
+      //     if(val) {
+      //       this.databaseReady.next(true);
+      //     } else {
+      //       this.fillDatabase();
+      //     }
+      //   });
+      // }, (error) => {
+      //   console.log('SQLite CREATE', error);
+      // })
     }, err => {
       console.log('PLATFORM READY', err);
     })
   }
 
+  initDatabase() {
+    this.sqlite.create({
+      name: 'monster.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+      this.database = db;
+    })
+    .catch((error) => {
+      console.log('ERROR: Init database', error);
+    })
+  }
+
   fillDatabase() {
-    this.http.get('assets/monsters.sql')
+    let url = '';
+    if(this.platform.isPlatformMatch('android')) {
+      console.log('Is Android');
+      url = '/android_asset/www/'
+    }
+    this.http.get(url + 'assets/monsters.sql')
       .map(res => res.text())
       .subscribe(sql => {
         this.sqlitePorter.importSqlToDb(this.database, sql)
@@ -50,12 +82,12 @@ export class DatabaseProvider {
             this.databaseReady.next(true);
             this.storage.set('database_filled', true);
           })
-          .catch(error => console.log('Import Sql To DB', error));
+          .catch(error => console.log('ERROR: Import Sql To DB', error));
       })
   }
 
   getAllMonster(){
-    return this.database.executeSql('SELECT * from monster', []).then((data) => {
+    return this.database.executeSql('SELECT * FROM monster', []).then((data) => {
       let monsters = [];
       if(data.rows.length > 0) {
         for(let i = 0; i < data.rows.length; i++) {
